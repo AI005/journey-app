63cdbc4f742222657af548fa6c39a8fa61e8ab1d
Phuong Nguyen <phuongcbnguyen@kms-technology.com>
2024-06-04 12:11:51 +0700

    add native project

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -2,13 +2,13 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Tab 1 - New version</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title size="large">This is my Tab 1 </ion-title>
         </ion-toolbar>
       </ion-header>
 
